04a99632f532ef57fb5e654bf0f42395f4914922
marcus <marcuseecl@gmail.com>
2022-03-20 15:45:31 +0800

    Add Coordinate helper

--- components/wall-component.tsx
+++ components/wall-component.tsx
@@ -1,7 +1,8 @@
 import { Props } from "next/script";
 import { useEffect } from "react"
 import { useAppSelector, useAppDispatch } from "../store/hooks";
-import { WallPixel, fetchFakeChunk } from "../store/reducer/wall.reducer";
+import { WallPixel, fetchFakeChunk, hoverOn } from "../store/reducer/wall.reducer";
+
 
 
 export default function Wall() {
@@ -9,7 +10,6 @@ export default function Wall() {
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-        console.log('asd');
         dispatch(fetchFakeChunk(BigInt(0)));
     }, []);
 
@@ -19,15 +19,23 @@ export default function Wall() {
     return (
         <div>
             <div className="flex mt-12 items-center w-full flex-col items-center">
-                <h2>The Wall</h2>
-                <WallChunk chunk={wallChunks.get(BigInt(0))} />
+                <div className="relative">
+                    <div className="hidden md:block sticky top-16 float-right mx-12 ">
+                        <WallCoordinateHelper />
+                    </div>
+                    <div className="hidden md:block sticky top-12 float-left mx-12">
+                        ➡️
+                    </div>
+                    <WallChunk chunk={wallChunks.get(BigInt(0))} />
+
+                </div>
             </div>
         </div>
     )
 }
 
 
-export function WallChunk({chunk}) {
+export function WallChunk({ chunk }) {
     useEffect(() => {
 
     }, []);
@@ -36,16 +44,42 @@ export function WallChunk({chunk}) {
         <div className="grid content-center grid-cols-32 gap-0 shadow-xl">
             {
                 chunk?.map((value: WallPixel, key: any) => {
-                    return <WallPixelUI key={key} pixel={value}/>
+                    return <WallPixelUI key={key} pixel={value} />
                 })
             }
         </div>
     )
 }
 
-export function WallPixelUI({pixel}) {
+export function WallPixelUI({ pixel }) {
     const dispatch = useAppDispatch();
 
-    return <div  onMouseEnter={() => console.log('a')} className="w-4 h-4 hover:border-black hover:border-2" style={{backgroundColor : pixel.colorString}}></div>
+    return <div onMouseEnter={() => dispatch(hoverOn(pixel))} className="w-4 h-4 hover:border-black hover:border-2" style={{ backgroundColor: pixel.colorString }}></div>
 }
 
+
+
+export function WallCoordinateHelper() {
+    const hoveredWallPixel = useAppSelector(state => state.wallReducer.hoveredWallPixel);
+
+    useEffect(() => {
+    }, []);
+
+    const getX = () => {
+        return hoveredWallPixel?.xPos.toString() ?? '';
+    }
+
+    const getY = () => {
+        return hoveredWallPixel?.yPos.toString() ?? '';
+    }
+
+    return (
+        <div className="flex flex-col">
+            <div>
+                <h4>Coordinate</h4>
+            </div>
+            <div > X : {getX()}</div>
+            <div > Y : {getY()}</div>
+        </div>
+    );
+}
