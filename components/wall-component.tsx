f4a431ad9c744ebac519101593fbb4a000b29e03
marcus <marcuseecl@gmail.com>
2022-04-03 17:08:27 +0800

    Format code

--- components/wall-component.tsx
+++ components/wall-component.tsx
@@ -7,85 +7,85 @@ import { WallPixel, fetchFakeChunk, hoverOn } from "../store/reducer/wall.reduce
 
 
 export default function Wall() {
-    const wallChunks = useAppSelector(state => state.wallReducer.wallChunks);
-    const dispatch = useAppDispatch();
+  const wallChunks = useAppSelector(state => state.wallReducer.wallChunks);
+  const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        dispatch(fetchFakeChunk(BigInt(0)));
-    }, []);
+  useEffect(() => {
+    dispatch(fetchFakeChunk(BigInt(0)));
+  }, []);
 
-    if (wallChunks.length === 0)
-        return <h2>Loading...</h2>
+  if (wallChunks.length === 0)
+    return <h2>Loading...</h2>
 
-    return (
-        <div>
-            <div className="flex mt-12 items-center w-full flex-col items-center">
-                <div className="relative">
-                    <div className="hidden md:block sticky top-16 float-right mx-12 ">
-                        <WallCoordinateHelper />
-                    </div>
-                    <div className="hidden md:block sticky top-16 float-left mx-12">
-                        ➡️
-                    </div>
-                    <WallChunk chunk={wallChunks[0]} />
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="flex mt-12 items-center w-full flex-col items-center">
+        <div className="relative">
+          <div className="hidden md:block sticky top-16 float-right mx-12 ">
+            <WallCoordinateHelper />
+          </div>
+          <div className="hidden md:block sticky top-16 float-left mx-12">
+            ➡️
+          </div>
+          <WallChunk chunk={wallChunks[0]} />
         </div>
-    )
-}
-
-
-export function WallChunk( {chunk} : { chunk : Array<WallPixel>  }) {
-    useEffect(() => {
-
-    }, []);
-
-    return (
-        <div className="grid content-center grid-cols-32 gap-0 shadow-xl">
-            {
-                chunk?.map((value: WallPixel, key: any) => {
-                    return <WallPixelUI key={key} pixel={value} />
-                })
-            }
-        </div>
-    )
-}
-
-export function WallPixelUI( {pixel} : { pixel : WallPixel }) {
-    const dispatch = useAppDispatch();
-    const router = useRouter();
-
-    return <div 
-        onMouseEnter={() => dispatch(hoverOn(pixel)) }  
-        onClick={() => router.push(`/pixel-detail/${pixel.id}`)} 
-        className="w-4 h-4 hover:border-black hover:border-2" 
-        style={{ backgroundColor: pixel.colorString }}>
+      </div>
     </div>
+  )
+}
+
+
+export function WallChunk({ chunk }: { chunk: Array<WallPixel> }) {
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <div className="grid content-center grid-cols-32 gap-0 shadow-xl">
+      {
+        chunk?.map((value: WallPixel, key: any) => {
+          return <WallPixelUI key={key} pixel={value} />
+        })
+      }
+    </div>
+  )
+}
+
+export function WallPixelUI({ pixel }: { pixel: WallPixel }) {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  return <div
+    onMouseEnter={() => dispatch(hoverOn(pixel))}
+    onClick={() => router.push(`/pixel-detail/${pixel.id}`)}
+    className="w-4 h-4 hover:border-black hover:border-2"
+    style={{ backgroundColor: pixel.colorString }}>
+  </div>
 }
 
 
 
 export function WallCoordinateHelper() {
-    const hoveredWallPixel = useAppSelector(state => state.wallReducer.hoveredWallPixel);
+  const hoveredWallPixel = useAppSelector(state => state.wallReducer.hoveredWallPixel);
 
-    useEffect(() => {
-    }, []);
+  useEffect(() => {
+  }, []);
 
-    const getX = () => {
-        return hoveredWallPixel?.xPos ?? '';
-    }
+  const getX = () => {
+    return hoveredWallPixel?.xPos ?? '';
+  }
 
-    const getY = () => {
-        return hoveredWallPixel?.yPos ?? '';
-    }
+  const getY = () => {
+    return hoveredWallPixel?.yPos ?? '';
+  }
 
-    return (
-        <div className="flex flex-col">
-            <div>
-                <h4>Coordinate</h4>
-            </div>
-            <div > X : {getX()}</div>
-            <div > Y : {getY()}</div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col">
+      <div>
+        <h4>Coordinate</h4>
+      </div>
+      <div > X : {getX()}</div>
+      <div > Y : {getY()}</div>
+    </div>
+  );
 }
